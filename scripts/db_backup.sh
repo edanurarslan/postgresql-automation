716d249a7bea827818a6cac532924e22d7d8dc4e
Eda Nur Arslan <116951700+edanurarslan@users.noreply.github.com>
2026-06-02 22:16:48 +0300

    Update db_backup.sh

--- scripts/db_backup.sh
+++ scripts/db_backup.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# --- AYARLAR ---
-export PGPASSWORD="5530"
+# --- SETTINGS ---
+export PGPASSWORD="PASSWORD"
 DB_NAME="food_roulette_db"
 USER_NAME="postgres"
 
-# Kullanıcı adını dinamik alarak masaüstü yolunu bulma
+# Username dynamically found on desktop
 DESKTOP_PATH="/Users/$(whoami)/Desktop/postgre_otomasyon"
 BACKUP_DIR="$DESKTOP_PATH/backups"
 LOG_DIR="$DESKTOP_PATH/logs"
 
-# Tarih ve Saat Formatı (Yıl-Ay-Gün_Saat-Dakika)
+# Date and time format
 TARIH=$(date +%Y-%m-%d_%H-%M)
 BACKUP_FILE="$BACKUP_DIR/${DB_NAME}_Backup_${TARIH}.backup"
 LOG_FILE="$LOG_DIR/Yedek_Log_${TARIH}.txt"
 
 echo "[$(date)] Yedekleme islemi baslatildi..." >> "$LOG_FILE"
 
-# pg_dump ile yedek alma komutu
+# back up pg_dump
 /Applications/pgAdmin\ 4.app/Contents/SharedSupport/pg_dump -U "$USER_NAME" -h localhost -F c -b -v -f "$BACKUP_FILE" "$DB_NAME" >> "$LOG_FILE" 2>&1
 
-# Hata Kontrolü
+# Error Handling
 if [ $? -eq 0 ]; then
     echo "[$(date)] BASARILI: Yedekleme sorunsuz tamamlandi." >> "$LOG_FILE"
 else
